5b7e7716e97ca2741053a087e012ebd861585249
ronniexrogers <ronniexrogers@yahoo.com>
2023-07-12 13:54:03 -0700

    initializing repo

---
+++ schemas/photo.ts
@@ -0,0 +1,12 @@
+export default {
+    name: 'photo',
+    type: 'image',
+      title: 'Photo',
+    fields: [
+      {
+        name: 'tile',
+        type: 'string',
+        title: 'Title'
+      }
+    ]
+  }
